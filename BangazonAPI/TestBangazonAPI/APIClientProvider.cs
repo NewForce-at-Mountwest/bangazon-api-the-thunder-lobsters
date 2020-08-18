fc025caa14eee086be43158cbccc7ae9afa631d9
Lindsey Crittendon <LindseyKCrittendon@gmail.com>
2020-08-18 16:11:38 -0400

    tests built, but they won't run due to build errors

--- BangazonAPI/TestBangazonAPI/APIClientProvider.cs
+++ BangazonAPI/TestBangazonAPI/APIClientProvider.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using BangazonAPI;
+using System.Net.Http;
+using Xunit;
 
 namespace TestBangazonAPI
 {
-    class APIClientProvider
+    class APIClientProvider : IClassFixture<WebApplicationFactory<Startup>>
     {
+        public HttpClient Client
+        {
+            get; private set;
+        }
+        private readonly WebApplicationFactory<Startup> _factory = new WebApplicationFactory<Startup>();
+
+        public APIClientProvider()
+        {
+            Client = _factory.CreateClient();
+        }
+
+        public void Dispose()
+        {
+            _factory?.Dispose();
+            Client?.Dispose();
+        }
     }
 }
--- BangazonAPI/TestBangazonAPI/DatabaseFixture.cs
+++ BangazonAPI/TestBangazonAPI/DatabaseFixture.cs
@@ -1,12 +1,69 @@
-﻿using System;
+﻿using BangazonAPI.Models;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Data.SqlClient;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace TestBangazonAPI
+namespace BangazonAPITests
 {
-    class DatabaseFixture
+    public class DatabaseFixture : IDisposable
     {
+
+        private readonly string ConnectionString = @$"Server=localhost\SQLEXPRESS;Database=BangazonAPI;Trusted_Connection=True;";
+
+
+
+        public Customer TestCustomer
+        {
+            get; set;
+        }
+
+        public DatabaseFixture()
+        {
+
+            Customer newCustomer = new Customer
+            {
+                FirstName = "Test name",
+                LastName = "Test name",
+                CreationDate = new DateTime(1234, 12, 12, 12, 12, 12),
+                LastActiveDate = new DateTime(1234, 12, 12, 12, 12, 12)
+            };
+
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @$"INSERT INTO Customer (FirstName, LastName, CreationDate, LastActiveDate)
+                                        OUTPUT INSERTED.Id
+                                        VALUES ('{newCustomer.FirstName}', '{newCustomer.LastName}', '{newCustomer.CreationDate}', '{newCustomer.LastActiveDate}')";
+
+
+                    int newId = (int)cmd.ExecuteScalar();
+
+                    newCustomer.Id = newId;
+
+                    TestCustomer = newCustomer;
+                }
+            }
+
+        }
+
+        public void Dispose()
+        {
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @$"DELETE FROM Customer WHERE FirstName='Test Name'";
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+
     }
 }
